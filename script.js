74783aa4bba0da0170e912228a9f5ae5ac43d739
Amdel <amdel.gordones.539@alu.tecnica29de6.edu.ar>
2025-11-24 21:32:07 -0300

    second commit

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-document.getElementById('translateButton').addEventListener('click', function() {
+document.getElementById('translateToBinaryButton').addEventListener('click', function() {
     const inputText = document.getElementById('inputText').value;
     const outputBinary = document.getElementById('outputBinary');
 
     // Traducción a binario
     let binaryResult = '';
     for (let i = 0; i < inputText.length; i++) {
-        binaryResult += inputText.charCodeAt(i).toString(2) + ' ';
+        binaryResult += inputText.charCodeAt(i).toString(2).padStart(8, '0') + ' ';
     }
 
     outputBinary.textContent = binaryResult.trim();
+});
+
+document.getElementById('translateToTextButton').addEventListener('click', function() {
+    const inputBinary = document.getElementById('inputBinary').value.trim();
+    const outputText = document.getElementById('outputText');
+    
+    // Traducción de binario a texto
+    const binaryArray = inputBinary.split(' ');
+    let textResult = '';
+    
+    for (let binary of binaryArray) {
+        textResult += String.fromCharCode(parseInt(binary, 2));
+    }
+
+    outputText.textContent = textResult;
 });
